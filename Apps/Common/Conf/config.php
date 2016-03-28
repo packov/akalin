37e701304ca082e6cbab4936564baa8d46280306
胡星 <xinghu@lingjing.com>
2016-03-28 16:37:16 +0800

    add:.htaccess文件
    URL重写隐藏应用的入口文件index.php,

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -1,7 +1,18 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    /*调试配置*/
     'SHOW_PAGE_TRACE' =>true,
+    //模块配置
+    'DEFAULT_MODULE'  => 'Home',
+    'URL_CASE_INSENSITIVE' => true,//url不区分大小写
+    'TAGLIB_BEGIN'=>'{',
+    'TAGLIB_END'=>'}',
+    // URL伪静态后缀设置
+    'URL_HTML_SUFFIX'   => '',
+
+    //统一生成小写的URL地址
+    'URL_CASE_INSENSITIVE' => true,
     /* 数据库设置 */
     'DB_TYPE'               =>  'mysqli',     // 数据库类型
     'DB_HOST'               =>  '127.0.0.1', // 服务器地址
